b367f364cfdf5f9142038e54bbb76f3f5bddf8fc
JAHIR ALAM <jahiralam241@gmail.com>
2023-03-11 00:06:06 +0530

    only code

--- linear_search_using_funtion.cpp
+++ linear_search_using_funtion.cpp
@@ -1,21 +1,15 @@
 #include<iostream>
 using namespace std;
-
-
 int  linear_search(int a[],int n,int key){
     for(int i=0;i<5;i++){
         if(key==a[i])
         return i;
     }
     return -1;
-
 }
 int main(){
     int a[5]={1,2,5,6,7};
     int index=linear_search(a,5,7);
     cout<<"element founded at"<<index<<endl;
-
-    
-    return 0;
-    
+     return 0; 
 }
